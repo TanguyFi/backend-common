a55a89b1be012e1ef19a5543a6ded4e091d8b6a8
ewoken <ewokenfr@gmail.com>
2018-02-03 18:13:38 +0100

    Initial commit

---
+++ src/formatMongoObject.js
@@ -0,0 +1,8 @@
+function formatMongoObject(object) {
+  return {
+    ...object,
+    _id: object._id.toString(), // eslint-disable-line no-underscore-dangle
+  };
+}
+
+export default formatMongoObject;
