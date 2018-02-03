6220a40fbdfb7423b9e518390efa6eecb8ef449c
firguy <tanguy@selectively.co>
2018-02-03 18:13:38 +0100

    Initial commit

---
+++ src/api/serviceToRoute.js
@@ -0,0 +1,9 @@
+function serviceToRoute(serviceFunction) {
+  return (req, res, next) =>
+    serviceFunction(req.body, req.user)
+      .then(serviceResult => res.json(serviceResult))
+      .then(() => next())
+      .catch(next);
+}
+
+export default serviceToRoute;
