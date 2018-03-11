5362f0b688c6dbd8fa489b17f72124a618cfec0e
ewoken <ewokenfr@gmail.com>
2018-03-11 11:13:37 +0100

    [Fix] Improve merge options in fetchApi

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,7 +1,7 @@
 import isoFetch from 'isomorphic-fetch';
 import fetchWithCookieBuilder from 'fetch-cookie';
 import { CookieJar } from 'tough-cookie';
-import { omit } from 'ramda';
+import { omit, mergeDeepRight } from 'ramda';
 import { DomainError } from './errors';
 
 const cookieJar = new CookieJar();
@@ -9,20 +9,17 @@ const fetchWithCookie = fetchWithCookieBuilder(isoFetch, cookieJar);
 
 async function fetchServer(url, options) {
   const fetch = options.cookie ? fetchWithCookie : isoFetch;
-  const fetchOptions = omit(['cookie'], options);
-
-  const result = await fetch(url, {
-    // TODO ramda deep merge
-    ...fetchOptions,
-    headers: {
-      'Content-Type': 'application/json',
-      ...fetchOptions.headers,
+  const fetchOptions = mergeDeepRight(
+    {
+      headers: { 'Content-Type': 'application/json' },
     },
-  });
+    omit(['cookie'], options),
+  );
+
+  const result = await fetch(url, fetchOptions);
   const body = await result.json();
   if (!result.ok) {
     if (result.status === 400) {
-      // TODO improve
       throw new DomainError(
         body.error.message,
         body.error.errorCode,
